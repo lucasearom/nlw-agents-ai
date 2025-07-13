2876aed491e020e8e444e62c21c9f1dbdde1408c
lucasearom <lucasmoraes211@gmail.com>
2025-07-13 13:03:27 -0300

    feat: capturando e verificando se há valores no input

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,19 @@ const askButton = document.getElementById("askButton");
 const aiResponse = document.getElementById("aiResponse");
 const form = document.getElementById("form");
 
+const enviarFormulario = (event) => {
+    event.preventDefault(); // método responsável por cancelar um evento, no caso prevenindo que o form recarregue após ser enviado
+
+    // capturando o valor no input
+    const apiKey = apiKeyInput.value;
+    const game = gameSelect.value;
+    const question = questionInput.value;
+
+    // condição para validar se os campos foram preenchidos
+    if (apiKey == "" || game == "" || question == "") {
+        alert("Por favor, preencha todos os campos");
+        return;
+    }
+};
+
 form.addEventListener("submit", enviarFormulario);
